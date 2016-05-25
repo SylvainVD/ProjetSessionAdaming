670272256493a033993b372b7d9bfdc5aee34954
Sylvain <INTI-0252@DESKTOP-PFSDC3D>
2016-05-25 14:54:43 +0200

    First Commit

---
+++ Adaming/src/main/java/com/Adaming/myapp/entities/Etudiant.java
@@ -0,0 +1,34 @@
+package com.Adaming.myapp.entities;
+
+import javax.persistence.Entity;
+
+/*
+ * Auteur : Sylvain VROLAND
+ * Date : 25/05/2016
+ * class : Etudiant
+ * package : com.Adaming.myapp.entities ;
+ * Version : 1.0 ;
+ * ref-uml : 1 ;
+ * sprint : 1 ;
+ * ref-userStories : unknown ;
+ * association :  	OneToMany unidirectionnelle List<Materiel>, 
+ * 					OneToMany List<Exament>, 
+ * 					ManyToOne Session;
+ * */
+
+@Entity
+public class Etudiant {
+	
+		// Attibuts
+	private Long idEtudiant;
+	private ;
+	
+
+		// Associations
+
+		// Getters & Setters
+
+		// Constructeurs
+
+
+}
